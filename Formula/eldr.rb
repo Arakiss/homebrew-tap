4b9a87e6062273dec5c7db876b159497aef83017
Arakiss <petruarakiss2025@gmail.com>
2026-06-06 00:07:03 +0200

    fix formula: std_cargo_args + dep order (brew audit --strict)

--- Formula/eldr.rb
+++ Formula/eldr.rb
@@ -7,12 +7,11 @@ class Eldr < Formula
   head "https://github.com/Arakiss/eldr.git", branch: "main"
 
   depends_on "rust" => :build
-  depends_on :macos
   depends_on arch: :arm64
+  depends_on :macos
 
   def install
-    system "cargo", "build", "--release", "--locked"
-    bin.install "target/release/eldr"
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
